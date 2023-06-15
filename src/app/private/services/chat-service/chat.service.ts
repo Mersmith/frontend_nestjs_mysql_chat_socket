a3b67db8ea5017c58bcf120320fdd632d376185a
Mersmith <mersmith14@gmail.com>
2023-06-15 15:57:25 -0500

    Socket comunicación con Backend

---
+++ src/app/private/services/chat-service/chat.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { CustomSocket } from '../../sockets/custom-socket';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(private socket: CustomSocket) { }
+
+  sendMessage() { }
+
+  getMessage() {
+    return this.socket.fromEvent('message');
+  }
+}
